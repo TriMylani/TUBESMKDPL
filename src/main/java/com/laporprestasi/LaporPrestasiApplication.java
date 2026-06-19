47d1e18a7b540722d0b33412e488a939667289d5
TriMylani <trimylani5@gmail.com>
2026-06-19 09:21:40 +0700

    first commit

---
+++ src/main/java/com/laporprestasi/LaporPrestasiApplication.java
@@ -0,0 +1,13 @@
+package com.laporprestasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaporPrestasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaporPrestasiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laporprestasi/LaporPrestasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laporprestasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaporPrestasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
